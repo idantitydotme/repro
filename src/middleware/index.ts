8d77d98ef3cf5f8f48c5eb54425640eb7f1601da
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-12 03:00:26 -0300

    fix: tests uncommenting api call

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,9 +2,9 @@ import { defineMiddleware } from "astro:middleware"
 import { auth as betterAuth } from "@/auth/auth"
 
 export const onRequest = defineMiddleware(async (context, next) => {
-  // const isAuthed = await betterAuth.api.getSession({
-  //   headers: context.request.headers,
-  // })
+  const isAuthed = await betterAuth.api.getSession({
+    headers: context.request.headers,
+  })
   // context.locals.user = isAuthed?.user ?? null
   // context.locals.session = isAuthed?.session ?? null
   return next()
