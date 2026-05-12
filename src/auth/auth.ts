e451b43d50db2e896a823c6e221aae678c8ca96e
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-12 02:57:30 -0300

    fix: tests readding config

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,3 +1,14 @@
 import { betterAuth } from "better-auth"
 
-export const auth = betterAuth({})
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    useSecureCookies: true,
+    cookiePrefix: "auth",
+    database: {
+      generateId: "uuid",
+    },
+  },
+})
