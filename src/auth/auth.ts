251452e62198f9c828eb611d5e73f21d226ddada
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-05-12 18:58:13 -0300

    fix: tests adding db

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,12 +1,13 @@
 import { betterAuth } from "better-auth"
 
-// import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { db } from "@/db"
 import { admin as adminPlugin, organization as organizationPlugin } from "better-auth/plugins"
 
 export const auth = betterAuth({
-  // database: drizzleAdapter(db, {
-  //   provider: "pg"
-  // }),
+  database: drizzleAdapter(db, {
+    provider: "pg"
+  }),
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
